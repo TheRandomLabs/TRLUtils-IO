4175767f03599307a1ab615e0d4acc28484a6aec
TheRandomLabs <therandomlabsinc@gmail.com>
2019-12-04 20:03:02 +1100

    Fix human readable sizes

--- src/main/java/com/therandomlabs/utils/io/HumanReadableSize.java
+++ src/main/java/com/therandomlabs/utils/io/HumanReadableSize.java
@@ -2,8 +2,20 @@ package com.therandomlabs.utils.io;
 
 /**
  * Contains utility methods for converting sizes in bytes to human-readable formats.
+ * The code in this class has been taken and adapted from
+ * <a href="https://programming.guide/java/formatting-byte-size-to-human-readable-format.html">
+ * here
+ * </a>.
  */
 public final class HumanReadableSize {
+	private static final String FORMAT_STRING = "%s%.1f %s";
+	private static final String[] DECIMAL_SYMBOLS = {
+			"B", "kB", "MB", "GB", "TB", "PB", "EB"
+	};
+	private static final String[] BINARY_SYMBOLS = {
+			"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"
+	};
+
 	private HumanReadableSize() {}
 
 	/**
@@ -15,7 +27,26 @@ public final class HumanReadableSize {
 	 * or {@code "1,0 MB"} depending on the locale.
 	 */
 	public static String decimal(long bytes) {
-		return get(bytes, true);
+		final String sign = bytes < 0 ? "-" : "";
+
+		//Math.abs(Long.MIN_VALUE) is Long.MIN_VALUE.
+		long abs = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+
+		if (abs < 1000L) {
+			return bytes + " " + DECIMAL_SYMBOLS[0];
+		}
+
+		for (int i = 1; i < DECIMAL_SYMBOLS.length - 1; i++) {
+			if (abs < 999_950L) {
+				return String.format(FORMAT_STRING, sign, abs / 1000.0, DECIMAL_SYMBOLS[i]);
+			}
+
+			abs /= 1000;
+		}
+
+		return String.format(
+				FORMAT_STRING, sign, abs / 1000.0, DECIMAL_SYMBOLS[DECIMAL_SYMBOLS.length - 1]
+		);
 	}
 
 	/**
@@ -27,22 +58,26 @@ public final class HumanReadableSize {
 	 * the string {@code "1.0 MiB"} is returned, or {@code "1,0 MiB"} depending on the locale.
 	 */
 	public static String binary(long bytes) {
-		return get(bytes, false);
-	}
-
-	//Taken from https://stackoverflow.com/a/3758880
-	private static String get(long bytes, boolean decimal) {
 		final String sign = bytes < 0 ? "-" : "";
-		final int unit = decimal ? 1000 : 1024;
-		bytes = Math.abs(bytes);
 
-		if (bytes < unit) {
-			return sign + bytes + " B";
+		//Math.abs(Long.MIN_VALUE) is Long.MIN_VALUE.
+		long abs = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+
+		if (abs < 1024L) {
+			return bytes + " " + BINARY_SYMBOLS[0];
 		}
 
-		final int exponent = (int) (Math.log(bytes) / Math.log(unit));
-		final String symbol =
-				(decimal ? "kMGTPE" : "KMGTPE").charAt(exponent - 1) + (decimal ? "" : "i");
-		return String.format("%s%.1f %sB", sign, bytes / Math.pow(unit, exponent), symbol);
+		for (int i = 1; i < BINARY_SYMBOLS.length - 1; i++) {
+			final double size = Math.pow(1024.0, i);
+
+			if (abs < size * 1024 - 52) {
+				return String.format(FORMAT_STRING, sign, abs / size, BINARY_SYMBOLS[i]);
+			}
+		}
+
+		return String.format(
+				FORMAT_STRING, sign, abs / Math.pow(1024.0, BINARY_SYMBOLS.length - 1.0),
+				BINARY_SYMBOLS[BINARY_SYMBOLS.length - 1]
+		);
 	}
 }
--- src/test/java/com/therandomlabs/utils/io/HumanReadableSizeTest.java
+++ src/test/java/com/therandomlabs/utils/io/HumanReadableSizeTest.java
@@ -7,21 +7,31 @@ import org.junit.jupiter.api.Test;
 public class HumanReadableSizeTest {
 	@Test
 	public void correctDecimalSizesShouldBeReturned() {
+		assertThat(HumanReadableSize.decimal(27)).isEqualTo("27 B");
+		assertThat(HumanReadableSize.decimal(999)).isEqualTo("999 B");
 		assertThat(HumanReadableSize.decimal(1000)).matches("1[.,]0 kB");
+		assertThat(HumanReadableSize.decimal(1023)).matches("1[.,]0 kB");
+		assertThat(HumanReadableSize.decimal(1024)).matches("1[.,]0 kB");
 		assertThat(HumanReadableSize.decimal(1500)).matches("1[.,]5 kB");
 		assertThat(HumanReadableSize.decimal(1000000)).matches("1[.,]0 MB");
 		assertThat(HumanReadableSize.decimal(1500000)).matches("1[.,]5 MB");
 		assertThat(HumanReadableSize.decimal(1000000000)).matches("1[.,]0 GB");
 		assertThat(HumanReadableSize.decimal(1550000000)).matches("1[.,]6 GB");
+		assertThat(HumanReadableSize.decimal(Long.MAX_VALUE)).matches("9[.,]2 EB");
 	}
 
 	@Test
 	public void correctBinarySizesShouldBeReturned() {
+		assertThat(HumanReadableSize.binary(27)).isEqualTo("27 B");
+		assertThat(HumanReadableSize.binary(999)).isEqualTo("999 B");
+		assertThat(HumanReadableSize.binary(1000)).matches("1000 B");
+		assertThat(HumanReadableSize.binary(1023)).matches("1023 B");
 		assertThat(HumanReadableSize.binary(1024)).matches("1[.,]0 KiB");
 		assertThat(HumanReadableSize.binary(1500)).matches("1[.,]5 KiB");
 		assertThat(HumanReadableSize.binary(1048576)).matches("1[.,]0 MiB");
 		assertThat(HumanReadableSize.binary(1500000)).matches("1[.,]4 MiB");
 		assertThat(HumanReadableSize.binary(1073741824)).matches("1[.,]0 GiB");
 		assertThat(HumanReadableSize.binary(1550000000)).matches("1[.,]4 GiB");
+		assertThat(HumanReadableSize.binary(Long.MAX_VALUE)).matches("8[.,]0 EiB");
 	}
 }
